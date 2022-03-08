baeaa031abc1a137902c830bfc3f5f5ea256d609
VictorRaffaele <victorraffaele@gmail.com>
2022-03-08 17:51:27 -0300

    Exercicios finais

--- Exercicios/Main.java
+++ Exercicios/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        TemperaturaMedia claseTemp = new TemperaturaMedia();
-        Double tempMedia = claseTemp.media(claseTemp.entradas());
-        System.out.println(tempMedia);
-        System.out.println(claseTemp.getEntradas());
-        claseTemp.maioresMedia(claseTemp.getEntradas(), tempMedia);
+      TemperaturaMedia claseTemp = new TemperaturaMedia();
+      Double tempMedia = claseTemp.media(claseTemp.entradas());
+      System.out.println(tempMedia);
+      System.out.println(claseTemp.getEntradas());
+      claseTemp.maioresMedia(claseTemp.getEntradas(), tempMedia);
 
-    }
+  }
 }
